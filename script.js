5ccd93ccdd8a6b99f15fde9a46584d288beb70f0
LIGECT <viminiko@gmail.com>
2025-04-23 06:57:11 +0300

    Finished section two. Getting somewhere, maybe.

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ document.querySelector(".logo").addEventListener("mouseover", function () {
 document.querySelector(".logo").addEventListener("mouseout", function () {
   this.style.animation = "float 3s ease-in-out infinite, pulse 2s infinite";
 });
+
+document.querySelector(".bh-logo").addEventListener("mouseover", function () {
+  this.style.transform = "scale(1.05) rotate(5deg)";
+});
+
+document.querySelector(".bh-logo").addEventListener("mouseout", function () {
+  this.style.transform = "scale(1) rotate(0)";
+});
+
+// Add some fun to the fun fact
+const funFact = document.querySelector(".fun-fact");
+funFact.addEventListener("click", function () {
+  this.style.transform = "scale(1.02)";
+  setTimeout(() => {
+    this.style.transform = "scale(1)";
+  }, 200);
+});
